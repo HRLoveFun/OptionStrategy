50e2dd6fd3f7a02e7e29eece9bc51ec919af863c
GradientDescent <hongru.chen@foxmail.com>
2025-09-19 19:26:58 +0800

    Refactor app.run to use port variable and enable debug
    Updated app.run to use a variable for port and enabled debug mode.

--- app.py
+++ app.py
@@ -73,6 +73,9 @@ def validate_ticker():
         return jsonify({'valid': False, 'message': f'Error validating ticker: {str(e)}'})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False) 
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port, debug=True)
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False) 
 
 application = app
